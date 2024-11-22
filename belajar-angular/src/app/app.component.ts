69d752e1ced4e9b4811e0dc68f27d2c5e2fc9c07
Djali Suandre <djalisuandre_2226240011@mhs.mdp.ac.id>
2024-11-22 11:22:14 +0700

    updates

--- belajar-angular/src/app/app.component.ts
+++ belajar-angular/src/app/app.component.ts
@@ -1,11 +1,13 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { RouterModule } from '@angular/router'; // impor RouterModule
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  standalone: true,
+  imports: [RouterOutlet, RouterModule], // tambahkan RouterOutlet dan RouterModule
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrl: './app.component.css',
 })
 export class AppComponent {
   title = 'belajar-angular';
